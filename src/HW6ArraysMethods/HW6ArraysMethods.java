4dfc42d449f77070447cc85c39984a2ac2797193
Joseph <jaathunder@yahoo.com>
2019-11-07 01:07:37 -0800

    update and fix normal method, add semantic checking, still need to update welford method

--- src/HW6ArraysMethods/HW6ArraysMethods.java
+++ src/HW6ArraysMethods/HW6ArraysMethods.java
@@ -38,46 +38,53 @@ public class HW6ArraysMethods {
         } catch (Exception e) {
             System.out.println("Invalid range or data type.");
         }
+
+        //handle min and max for linear interpolation
         if (min > max) {
             float x = max;
             max = min;
             min = x;
         }
 
-        //generate random numbers and store it in an array
-        double[] a = new double[N];
-        for (int i = 0; i < a.length; i++) {
-            //this is linear interpolation
-            double rand = min + (Math.abs(max - min) * r.nextDouble());
-            a[i] = rand;
-            System.out.println(a[i]);
-        }
+        //do semantic checking for vars before processing
+        if (N == 1 || N == 0) {
+            System.out.println("N cannot be 0 or 1.");
+        }else if (min == max) {
+            System.out.println("min and max must be different values.");
+        }else{
+            //generate random numbers and store it in an array
+            double[] a = new double[N];
+            for (int i = 0; i < a.length; i++) {
+                //this is linear interpolation
+                double rand = min + (Math.abs(max - min) * r.nextDouble());
+                a[i] = rand;
+                System.out.println(a[i]);
+            }
 
-        //output the result using both std methods
-        System.out.println("Standard deviation using Standard method: " + stdS(a));
-        System.out.println("Standard deviation using  Welford method: " + stdW(a));
+            //output the result using both std methods
+            System.out.println("Standard deviation using Standard method: " + stdS(a));
+            System.out.println("Standard deviation using  Welford method: " + stdW(a));
+        }
     }
 
     public static double stdS(double[] a) {
         double sum1 = 0;
         double sum2 = 0;
-        int N = a.length;
+        double N = a.length;
 
         for (double v : a) {
-            sum1 =+ Math.pow(v, 2);
+            sum1 = sum1 + Math.pow(v, 2);
         }
-        sum1 = sum1 / N;
+        double sumA = sum1 / N;
 
         for (double v : a) {
-            sum2 =+ v;
+            sum2 = sum2 + v;
         }
-        sum2 = sum2 / N;
-        sum2 = Math.pow(sum2, 2);
-
-        return Math.sqrt(sum1 - sum2);
+        double sumB = Math.pow((sum2 / N), 2);
+        return Math.sqrt(sumA - sumB);
     }
 
-    public static double stdW(double a[]) {
+    public static double stdW(double[] a) {
         double mean = 0;
         double mean2 = 0;
         double delta;
@@ -88,7 +95,7 @@ public class HW6ArraysMethods {
             j = j + 1;
             delta = v - mean;
             mean = mean + (delta / j); //change to N
-            mean2 = mean2 + delta * (v - mean);
+            mean2 = mean2 + (delta * (v - mean));
         }
         return Math.sqrt(mean2 / (N - 1));
     }
