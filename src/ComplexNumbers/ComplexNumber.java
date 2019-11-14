8fdd6cd2873b4a8f3f46da8c9f1a4af01c862daa
Joseph <jaathunder@yahoo.com>
2019-11-14 12:51:21 -0800

    update hw7 for getters and setters, private vars, and functions

--- src/ComplexNumbers/ComplexNumber.java
+++ src/ComplexNumbers/ComplexNumber.java
@@ -2,46 +2,55 @@ package ComplexNumbers;
 
 public class ComplexNumber {
 
-    public ComplexNumber() { // constructs a complex number 0 + 0i
+    private double real;
+    private double imaginary;
+
+    //constructors
+    public ComplexNumber() {
+        // constructs a complex number 0 + 0i
+
+    }
+    public ComplexNumber(double _r, double _i) {
+        //constructs a complex number _r + _ii
+
+    }
+    public ComplexNumber(ComplexNumber rhs) {
+        //constructs a complex number rhs.r + rhs.ii
 
     }
 
-    public ComplexNumber(double _r, double _i) {//constructs a complex number _r + _ii
-
-    }
-    public ComplexNumber(ComplexNumber rhs) {//constructs a complex number rhs.r + rhs.ii
-
-    }
+    //setters
     public void setReal(double r) {
-
+        this.real = r;
     }
     public void setImag(double i) {
-
+        this.imaginary = i;
     }
+    //getters
     public double getReal() {
-
+        return this.real;
     }
     public double getImag() {
-
+        return this.imaginary;
     }
 
-    @Override
-    public String toString() {//returns a String formatted as such: 27.3 + 19.7𝑖.
-        //Note that if the imaginary part is negative, it should print a – (subtraction) rather than
-        //the addition of a negative number. E.g. 27.3 − 19.7𝑖 not 27.3 ± 19.7𝑖.
+/*
+    //arithmetic functions
+    public ComplexNumber add(ComplexNumber rhs) {
+        //add rhs to this
 
     }
-    public ComplexNumber add(ComplexNumber rhs) {//add rhs to this
+    public ComplexNumber sub(ComplexNumber rhs) {
+        //subtracts rhs from this
 
     }
-    public ComplexNumber sub(ComplexNumber rhs) {//subtracts rhs from this
-
-    }
-    public ComplexNumber mult(ComplexNumber rhs) {//multiplies rhs by this
+    public ComplexNumber mult(ComplexNumber rhs) {
+        //multiplies rhs by this
 
     }
     public ComplexNumber div(ComplexNumber rhs) throws ArithmeticException {
         //divides this by rhs, throws exception if division by 0 + 0i
+
     }
     public double mag () {//returns the magnitude of this
 
@@ -52,9 +61,23 @@ public class ComplexNumber {
     public ComplexNumber sqrt () {//returns the square root of this
 
     }
+
+    //comparator
     public boolean equals (ComplexNumber rhs) {//returns true if this is equal to rhs, false otherwise
 
     }
+*/
+
+/*
+    @Override
+    public String toString() {
+        //returns a String formatted as such: 27.3 + 19.7𝑖.
+        //Note that if the imaginary part is negative, it should print a – (subtraction) rather than
+        //the addition of a negative number. E.g. 27.3 − 19.7𝑖 not 27.3 ± 19.7𝑖.
+
+
+    }
+*/
 
 
     /*
