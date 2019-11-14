2af808b895a16da3a2bc510346f3dc232e96c976
Joseph <jaathunder@yahoo.com>
2019-11-14 09:25:29 -0800

    add hw info

--- src/HW7ComplexNumbers/HW7ComplexNumbers.java
+++ src/HW7ComplexNumbers/HW7ComplexNumbers.java
@@ -4,7 +4,18 @@ public class HW7ComplexNumbers {
 
     public static void main(String[] args) {
 
+        /*
 
+        printing
+        ie 27
+
+       im -13
+
+       27 + -13i
+       27 - 13i
+
+       27 - 13i
+         */
 
     }
 
