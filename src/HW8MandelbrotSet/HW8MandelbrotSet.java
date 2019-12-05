b8c8b0fafe08bfed7b426cece5672fb5e9f8d85c
Joseph <jaathunder@yahoo.com>
2019-12-04 21:38:26 -0800

    add image writer method

--- src/HW8MandelbrotSet/HW8MandelbrotSet.java
+++ src/HW8MandelbrotSet/HW8MandelbrotSet.java
@@ -1,5 +1,9 @@
 package HW8MandelbrotSet;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HW8MandelbrotSet {
@@ -19,6 +23,29 @@ public class HW8MandelbrotSet {
             System.out.println("Invalid input.");
         } finally {
             s.close();
+        }
+    }
+
+    public static void ImageWrite(int[][] img, String filename) throws IOException
+    {
+        try {
+            BufferedImage bi = new BufferedImage(img[0].length, img.length, BufferedImage.TYPE_INT_RGB);
+
+            // -- prepare output image
+            for (int i = 0; i < bi.getHeight(); ++i) {
+                for (int j = 0; j < bi.getWidth(); ++j) {
+                    int pixel =	(img[i][j] << 16) | (img[i][j] << 8) | (img[i][j]);
+//	    			int pixel =	((int)(Math.random() * 255) << 16) | (img[i][j] << 8) | (img[i][j]);
+                    bi.setRGB(j, i, pixel);
+                }
+            }
+
+            // -- write output image
+            File outputfile = new File(filename);
+            ImageIO.write(bi, "png", outputfile);
+        }
+        catch (IOException e) {
+            throw e;
         }
     }
 
